24581de626bc4d21d5bd3d00894c8983d0de6544
gyatthunter <soumodityapal2004@gmail.com>
2026-02-28 07:03:00 -0500

    Student details

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,10 +1,25 @@
 import java.util.Scanner;
-
 public class StudentDetails {
      public static void main(String[]args){
         Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter Your Name: ");
+        String name = input.nextLine();
 
-        int name = input.nextInt();
-        System.out.println("Your Name: " +name); 
+        System.out.println("Enter Your Roll Number: ");
+        int roll = input.nextInt();
+
+        System.out.println("Enter Your Age: ");
+        int age = input.nextInt();
+
+        System.out.println("Enter Your Marks: ");
+        double marks = input.nextDouble();
+        System.out.println("");
+
+        System.out.println("Name - " +name);
+        System.out.println("Roll - " +roll);
+        System.out.println("Age - " +age);
+        System.out.println("Marks - " +marks);
+        
      }
 }

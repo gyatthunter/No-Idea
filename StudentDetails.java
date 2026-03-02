1fb88e9a7f1233ebf7f3a2d20a4953a170548e6e
gyatthunter <soumodityapal2004@gmail.com>
2026-03-02 17:41:56 -0500

    Dhon khaba?

--- StudentDetails.java
+++ StudentDetails.java
@@ -15,7 +15,7 @@ public class StudentDetails {
         System.out.println("Enter Your Marks: ");
         double marks = input.nextDouble();
         
-        System.out.println("\n\nj"); // /n is for twogit  space. /n/n is for 3
+        System.out.println("\n\nj"); // /n is for two spacesgit. /n/n is for 3
 
         System.out.println("Name - " +name);
         System.out.println("Roll - " +roll);
---
+++ inigga.java
@@ -0,0 +1,8 @@
+public class inigga {
+    public static void main(){
+        double a = 10 * 0.20;
+        double b = 5 * .50;
+        double nigga  = a * b;
+        System.out.println("JI - " +nigga);
+    }
+}

46be2ee0cb3b9e226a08e2827ef0c8a88cd516c8
gyatthunter <soumodityapal2004@gmail.com>
2026-03-04 16:59:52 -0500

    scrapped

--- EmployeeSalary.java
+++ EmployeeSalary.java
@@ -1,1 +1,15 @@
 import java.util.Scanner;
+public class EmployeeSalary {
+
+    public static double Basic_salary(String[] args) {
+        Scanner dhon = new Scanner(System.in);
+        System.out.println("Enter Salary: ");
+        return dhon.nextDouble();
+    }
+    public static double Calcualte_hra(double basic) { //hra calculation//
+        return basic * 0.20;
+    }
+    public static double calcualte_da(double basic) { //da calculation//
+        return basic * .10;
+    }
+}
